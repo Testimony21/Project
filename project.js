dab0dd51fb90080592e894688d15451faa07f37c
Testimony Okocha <testimonyokocha@yahoo.com>
2025-02-18 21:19:31 +0100

    A div added

--- project.js
+++ project.js
@@ -1,17 +1,17 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//     let count = 1;
-//     let target = 16;
-//     let speed = 100;
+document.addEventListener("DOMContentLoaded", function(){
+    let count = 1;
+    let target = 16;
+    let speed = 100;
 
-//     let interval = setInterval(() =>{
-//         if (count < target){
-//             count++;
-//             document.getElementById("experience-number").textContent = count;
-//         }else{
-//             clearInterval(interval);
-//         }
-//     }, speed);
-// });
+    let interval = setInterval(() =>{
+        if (count < target){
+            count++;
+            document.getElementById("experience-number").textContent = count;
+        }else{
+            clearInterval(interval);
+        }
+    }, speed);
+});
 
 
 // console.log (document.querySelector('.hub').style.backgroundColor = 'green');
