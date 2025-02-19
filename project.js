a5061d3342d528c524af59a9da4d9b6ddceaaf76
Testimony Okocha <testimonyokocha@yahoo.com>
2025-02-19 18:39:45 +0100

    progress-bars added

--- project.js
+++ project.js
@@ -13,5 +13,45 @@ document.addEventListener("DOMContentLoaded", function(){
     }, speed);
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+    const progressBars = document.querySelectorAll(".progress-bar");
+
+    const observer = new  IntersectionObserver(entries =>{
+        entries.forEach(entry =>{
+            if (entry.isIntersecting){
+                let bar = entry.target;
+                let percent = bar.getAttribute("data-percent");
+                let count = 0;
+                let interval = setInterval(()=>{
+                    if (count >=percent){
+                        clearInterval(interval);
+                    }else{
+                        count++;
+                        bar.style.width = count + "%";
+                        bar.parentElement.previousElementSibling.querySelector(".percentage").textContent = count + "%";
+                    }
+                }, 20);
+                observer.unobserve(bar);
+            }
+        });
+    }, {threshold: 0.5});
+    progressBars.forEach(bar => observer.observe(bar));
+
+    // progressBars.forEach(bar =>{
+    //     let percent = bar.getAttribute("data-percent");
+    //     let count = 0;
+    //     let interval = setInterval(() =>{
+    //         if (count >= percent){
+    //             clearInterval(interval);
+    //         }else{
+    //             count++;
+    //             bar.style.width = count + "%"
+    //             bar.parentElement.previousElementSibling.querySelector(".percentage").textContent = count + "%"
+    //         }
+    //     }, 20);
+    //     observer.unobserve(bar);
+    // })
+})
+
 
 // console.log (document.querySelector('.hub').style.backgroundColor = 'green');
